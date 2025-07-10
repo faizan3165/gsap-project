4a5a0bc9dac4477b62e2495fe21d9849c2c07c8e
faizan3165 <faizanmehmood3165@gmail.com>
2025-07-11 01:25:52 +0500

    implement about section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { ScrollTrigger, SplitText } from "gsap/all";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Cocktails from "./components/Cocktails";
+import About from "./components/About";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -15,6 +16,8 @@ const App = () => {
       <Hero/>
 
       <Cocktails/>
+
+      <About/>
     </main>
   );
 };
